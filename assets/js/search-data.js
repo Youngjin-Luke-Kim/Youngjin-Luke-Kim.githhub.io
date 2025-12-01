6137c4d6878eb3f14c121ec7816ee42e783ef8e0
Youngjin-Luke-Kim <ykim9554@usc.edu>
2025-12-01 20:38:05 +0000

    Deploying to gh-pages from @ Youngjin-Luke-Kim/Youngjin-Luke-Kim.github.io@93deb9e6b8e7789650bea7ad6657e3aa1b952a0c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,16 +49,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-graduated-from-yonsei-university-bachelor",
+          title: 'Graduated from Yonsei University (Bachelor)',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-embarked-on-new-journey-at-sccad-under-prof-sungkyu-lim",
+          title: 'Embarked on new journey at SCCAD under Prof. Sungkyu Lim',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
